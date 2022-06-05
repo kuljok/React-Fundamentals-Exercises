9fa83d2b5c0c2012d484e248158644410e290c14
Andrei Kuleuski <kuljok@gmail.com>
2022-06-05 20:05:56 +0300

    Exercise: Controller Components I

--- L3. State Management/4. Controlled Components I/Starter/src/App.js
+++ L3. State Management/4. Controlled Components I/Starter/src/App.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
+  const [text, setText] = useState("");
+
+  const updateText = (text) => setText(text);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +14,9 @@ const App = () => {
         <h1 className="App-title">ReactND - Coding Practice</h1>
       </header>
       <div className="container">
-        <input type="text" placeholder="Say Something" />
+        <input type="text" placeholder="Say Something" onChange={(event) => updateText(event.target.value)} />
         <p className="echo">Echo:</p>
-        <p>This should mirror the text you typed into the input field.</p>
+        <p>{text}</p>
       </div>
     </div>
   );
