081732d519261a395673cc8c66d49e40b9e71c99
Andrei Kuleuski <kuljok@gmail.com>
2022-06-26 23:15:34 +0300

    Remove logo.svg

--- L3. State Management/6. All Together/Starter/src/App.js
+++ L3. State Management/6. All Together/Starter/src/App.js
@@ -1,10 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
 import AddUserForm from "./AddUserForm.js"
+import UserList from "./UserList.js"
+import {useState} from "react"
 
 const App = () => {
 
+  const [users, setUsers] = useState([])
+
   const addUser = (firstName, lastName, username) => {
+    const newUsers = [
+      ...users,
+      {
+        username: username,
+        firstName: firstName,
+        lastName: lastName,
+        playedGames: 0
+      }
+    ]
   }
 
   return (
@@ -17,16 +29,8 @@ const App = () => {
        <AddUserForm onNewUser={addUser} />
      </div>
      <hr className="my-4"></hr>
-      <div className="list-group list-group-flush border-bottom scrollarea">
-        <a href="#" class="list-group-item list-group-item-action active py-3 lh-tight" aria-current="true">
-          <div class="d-flex w-100 align-items-center justify-content-between">
-            <strong class="mb-1">List group item heading</strong>
-            <small>Wed</small>
-          </div>
-          <div class="col-10 mb-1 small">Some placeholder content in a paragraph below the heading and date.</div>
-        </a>
-      </div>
-    </div>
+     <UserList users={users} />
+   </div>
   );
 };
 
