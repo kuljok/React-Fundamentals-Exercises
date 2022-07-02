40a300f51a4f5ad874393f53a2bdc98406874e6f
Andrei Kuleuski <kuljok@gmail.com>
2022-07-02 15:52:15 +0300

    Show the appropriate messages for evety user itme when Show Numbers toggle is switched

--- L3. State Management/6. All Together/Starter/src/UserList.js
+++ L3. State Management/6. All Together/Starter/src/UserList.js
@@ -1,8 +1,8 @@
 import UserListItem from './UserListItem.js'
 
-const UserList = ({users =[]}) => (
+const UserList = ({users =[], showNumbers = true}) => (
   <div className="list-group list-group-flush border-bottom scrollarea">
-  {(users.map(user => <UserListItem key={user.username} {...user} />))}
+  {(users.map(user => <UserListItem key={user.username} showNumbers={showNumbers} {...user} />))}
   </div>
 )
 
--- L3. State Management/6. All Together/Starter/src/UserListItem.js
+++ L3. State Management/6. All Together/Starter/src/UserListItem.js
@@ -1,12 +1,17 @@
-const UserListItem = ({username, firstName, lastName, playedGames =0}) => 
-(
-  <a href="#" className="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
-    <div className="d-flex w-100 align-items-center justify-content-between">
-      <strong className="mb-1">{username}</strong>
-      <small>{playedGames}</small>
-    </div>
-    <div className="col-10 mb-1 small">{firstName}&nbsp;{lastName}</div>
-  </a>
-)
+const UserListItem = ({username, firstName, lastName, playedGames =0, showNumbers =true}) => 
+{
+  const playedGamesMessage = () => showNumbers ? `${username} played ${playedGames} games` :
+    `${username} played * games`;
+  return (
+      <a href="#" className="list-group-item list-group-item-action py-3 lh-tight" aria-current="true">
+        <div className="d-flex w-100 align-items-center justify-content-between">
+          <strong className="mb-1">{username}</strong>
+          <small>{playedGamesMessage()}</small>
+        </div>
+        <div className="col-10 mb-1 small">{firstName}&nbsp;{lastName}</div>
+      </a>
+    )
+}
+
 
 export default UserListItem;
