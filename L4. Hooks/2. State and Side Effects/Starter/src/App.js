fb69025b7efa1ddd7b2d81b4c10a010c5b76c35d
Andrei Kuleuski <kuljok@gmail.com>
2022-07-09 19:36:38 +0300

    Add scores state and move numCorrect and numQuestions to this new scores state

--- L4. Hooks/2. State and Side Effects/Starter/src/App.js
+++ L4. Hooks/2. State and Side Effects/Starter/src/App.js
@@ -1,14 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Equation from "./Equation.js"
+import {useState} from 'react'
 
 const value1 = Math.floor(Math.random() * 100);
 const value2 = Math.floor(Math.random() * 100);
 const value3 = Math.floor(Math.random() * 100);
 const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
 
 const App = () => {
+
+  const [scores, setScores] = useState({numCorrect: 0, numQuestions: 0})
+
+  const answer = (res) => {
+    const newScores = {...scores, 
+      numCorrect: res == value1 + value2 + value3 ? scores.numCorrect + 1 : scores.numCorrect,
+      numQuestions: scores.numQuestions + 1
+    }
+    setScores(newScores);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,13 +28,13 @@ const App = () => {
       </header>
       <div className="game">
         <h2>Mental Math</h2>
-        <div className="equation">
-          <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
-        </div>
+
+        <Equation a={value1} b={value2} c={value3} x={proposedAnswer}/>
+
         <button>True</button>
         <button>False</button>
         <p className="text">
-          Your Score: {numCorrect}/{numQuestions}
+          Your Score: {scores.numCorrect}/{scores.numQuestions}
         </p>
       </div>
     </div>
