6de95a359498e365d1a006f84fd7dba38ae377f9
Andrei Kuleuski <kuljok@gmail.com>
2022-07-10 12:26:06 +0300

    Add side efect cleanup to get rid off console warning

--- L4. Hooks/3. Side Effect Cleanup/Starter/src/ChildComponent.js
+++ L4. Hooks/3. Side Effect Cleanup/Starter/src/ChildComponent.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 
 function ChildComponent() {
   const [color, setColor] = useState("red");
+  let timer = null;
 
   useEffect(() => {
-    setTimeout(() => setColor("green"), 3000);
+    timer = setTimeout(() => setColor("green"), 3000);
+
+    return () => timer !== null && clearTimeout(timer);
   });
 
   return <p style={{ color }}>{color}</p>;
